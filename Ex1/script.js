9d7d1c053a8d67a9a9cf875a587736190d2f4128
mohamed mhamdi <medps4fr@gmail.com>
2025-02-12 22:54:30 +0100

    Create basic guess game

---
+++ Ex1/script.js
@@ -0,0 +1,35 @@
+class Game {
+    constructor(difficulte) {
+        this.difficulte = 4 - difficulte;
+        this.limit_int = 100 * difficulte;
+        this.nb_guesses = 10 * this.difficulte;
+        this.number_to_guess = Math.random() * 2 * this.limit_int - this.limit_int;
+    }
+
+    guess() {
+        if(this.nb_guesses===0) {
+            alert("You lost");
+            return false;
+        } else {
+            let answer = prompt(`Le nombre est entre ${-this.limit_int} et ${this.limit_int} le nombre de tentative restante est ${this.nb_guesses}`);
+            answer = Number(answer);
+            if(answer==this.number_to_guess) {
+                alert("You win");
+            }
+            this.nb_guesses -= 1;
+            return true;
+        }
+    }
+
+    game_start() {
+        while(this.guess());
+    }
+}
+let msg = "0";
+let dificulte;
+while(!([1, 2, 3].includes(Number(msg)))) {
+    msg = prompt("Choisir la difficulté : facile:1, intermédiaire:2, difficile:3");
+}
+difficulte = Number(msg);
+game = new Game(difficulte);
+game.game_start();
