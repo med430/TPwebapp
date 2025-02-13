9160b0c4ecab18907b16c898a731dfc001639e66
mohamed mhamdi <medps4fr@gmail.com>
2025-02-13 15:03:25 +0100

    Compeleted paragraph editor

--- Ex2/script.js
+++ Ex2/script.js
@@ -17,3 +17,21 @@ fonts.forEach((f) => {
     new_option.textContent = f;
     sel_parent.appendChild(new_option);
 })
+
+let color_picker = document.querySelector("#color-picker");
+
+let size_picker = document.querySelector("#size-picker");
+
+let paragraph_writer = document.querySelector("#paragraph_writer");
+
+color_picker.addEventListener("change", (e) => {
+    paragraph_writer.style.color = color_picker.value;
+});
+
+size_picker.addEventListener("change", (e) => {
+    paragraph_writer.style.fontSize = size_picker.value + "px";
+});
+
+sel_parent.addEventListener("change", (e) => {
+    paragraph_writer.style.fontFamily = sel_parent.value;
+})
