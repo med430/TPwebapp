9dd184dfad158b2e76b58d6061a2838abf5947de
mohamed mhamdi <medps4fr@gmail.com>
2025-02-16 16:00:54 +0100

    Minor change

--- Ex2/script.js
+++ Ex2/script.js
@@ -24,14 +24,19 @@ let size_picker = document.querySelector("#size-picker");
 
 let paragraph_writer = document.querySelector("#paragraph_writer");
 
+let test_figure = document.querySelector("h1");
+
 color_picker.addEventListener("change", (e) => {
     paragraph_writer.style.color = color_picker.value;
+    test_figure.style.color = color_picker.value;
 });
 
 size_picker.addEventListener("change", (e) => {
     paragraph_writer.style.fontSize = size_picker.value + "px";
+    test_figure.style.fontSize = size_picker.value + "px";
 });
 
 sel_parent.addEventListener("change", (e) => {
     paragraph_writer.style.fontFamily = sel_parent.value;
+    test_figure.style.fontFamily = sel_parent.value;
 })
